b231c93a92fd4f96289aa10113ce3a3ce950acb2
Kamador <kateamador13@gmail.com>
2022-08-26 23:30:28 -0500

    [header]: se implementa el archivo header

--- php/crudProveedor/proveedor.vista.php
+++ php/crudProveedor/proveedor.vista.php
@@ -12,119 +12,40 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">TIENDA</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
-                    </li>
-                    <!-- <li class="nav-item">
-                <a class="nav-link" href="#">Productos</a>
-              </li> -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Productos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="agregarProducto.html">Agregar</a></li>
-                            <li><a class="dropdown-item" href="modificarProducto.html">Modificar</a></li>
-                            <!-- <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Empleados
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="AgregarEmpleados.html">Agregar</a></li>
-                            <!-- <li><a class="dropdown-item" href="modificarProducto.html">Modificar</a></li> -->
-                            <!-- <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Cliente
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Agregar</a></li>
-                            <!-- <li><a class="dropdown-item" href="modificarProducto.html">Modificar</a></li> -->
-                            <!-- <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                        </ul>
-                    </li>
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Proveedor
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="agregarProveedor.html">Agregar </a></li>
-                            <!-- <li><a class="dropdown-item" href="modificarProducto.html">Modificar</a></li> -->
-                            <!-- <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Ventas
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="AgregarVentas.html">Agregar</a></li>
-                            <!-- <li><a class="dropdown-item" href="modificarProducto.html">Modificar</a></li> -->
-                            <!-- <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <?php include '../util/header.php'; ?>
 
-    <!-- <div class="card-body p-md-5">
-              <div class="row justify-content-center">
-                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1"> -->
     <div class="container-fluid">
         <div class="row">
             <div class="card offset-md-4 col-md-4">
-                <form>
+                <form method="post" action="proveedor.controlador.php">
                     <div class="mb-3">
-                        <label for="exampleFormControgitlInput1" class="form-label">Empresa</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre Empresa">
+                        <label for="nombreEmpresa" class="form-label">Empresa</label>
+                        <input type="text" class="form-control" id="nombreEmpresa" name="nombreEmpresa" placeholder="Nombre Empresa">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Direccion</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Direccion">
+                        <label for="direccion" class="form-label">Direccion</label>
+                        <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Direccion">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Telefono</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Telefono">
+                        <label for="telefono" class="form-label">Telefono</label>
+                        <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Asesor</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre Asesor">
+                        <label for="asesor" class="form-label">Asesor</label>
+                        <input type="text" class="form-control" id="asesor" name="asesor" placeholder="Nombre Asesor">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Telefono Asesor</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Telefono Asesor">
+                        <label for="telAsesor" class="form-label">Telefono Asesor</label>
+                        <input type="text" class="form-control" id="telAsesor" name="telAsesor" placeholder="Telefono Asesor">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label">Informacion Adicional</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <label for="infoAdicional" class="form-label">Informacion Adicional</label>
+                        <textarea class="form-control" id="infoAdicional" name="infoAdicional" rows="3"></textarea>
                     </div>
                     <div class="mb-3">
                         <input class="btn btn-primary" type="submit" value="Submit">
