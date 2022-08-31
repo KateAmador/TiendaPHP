b139dc10bcc9fdd0e0f30aea2005ebc2dc730086
Kamador <kateamador13@gmail.com>
2022-08-31 12:52:58 -0500

    [Proveedores]: se implementa guardar

--- php/crudProveedor/proveedor.controlador.php
+++ php/crudProveedor/proveedor.controlador.php
@@ -1,26 +1,23 @@
 <?php
+ 
+    include_once('../conexion.php');
+    $nombreEmpresa = trim($_POST['nombreEmpresa']);
+    $direccion = trim($_POST['direccion']);
+    $telefono = trim($_POST['telefono']);
+    $email = trim($_POST['email']);
+    $asesor = trim($_POST['asesor']);
+    $telAsesor = trim($_POST['telAsesor']);
+    $infoAdicional = trim($_POST['infoAdicional']);
 
-$nombreEmpresa = $_REQUEST['nombreEmpresa'];
-$direccion = $_REQUEST['direccion'];
-$telefono = $_REQUEST['telefono'];
-$email = $_REQUEST['email'];
-$asesor = $_REQUEST['asesor'];
-$telAsesor = $_REQUEST['telAsesor'];
-$infoAdicional = $_REQUEST['infoAdicional'];
 
-echo $nombreEmpresa;
-echo $direccion;
-echo $telefono;
-echo $email;
-echo $asesor;
-echo $telAsesor;
-echo $infoAdicional;
-
-function GuardarProveedor(){
-
-}
-
-header("Location:proveedor.vista.php");
-
+    $sql= "INSERT INTO proveedores(empresa_proveedor, direccion_proveedor, telefono_proveedor, email_proveedor, asesor_proveedor, telAsesor_proveedor, infoAdicional_proveedor)
+             VALUES ('$nombreEmpresa','$direccion','$telefono','$email','$asesor','$telAsesor','$infoAdicional')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Proveedor creado correctamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    
+    mysqli_close($conn);
 
 ?>
--- php/crudProveedor/proveedor.vista.php
+++ php/crudProveedor/proveedor.vista.php
@@ -13,55 +13,57 @@
 
 <body>
     <?php include '../util/header.php'; ?>
-
-    <form class="card col-12 col-md-6 offset-md-3 mt-5" method="post" action="proveedor.controlador.php">
+    <form class="card col-12 col-md-6 offset-md-3 mt-5" method="POST" action="proveedor.controlador.php">
         <div class="card-header">
             <h5 class="card-title">Agregar Proveedor</h5>
         </div>
-        
         <div class="card-body p-md-5">
             <div class="row justify-content-center">
                 <div>
                     <div class="row">
                         <div class="mb-3 col-6">
                             <label for="nombreEmpresa" class="form-label">Empresa</label>
-                            <input type="text" class="form-control" id="nombreEmpresa" name="nombreEmpresa" placeholder="Nombre Empresa">
+                            <input type="text" class="form-control" id="nombreEmpresa" name="nombreEmpresa" placeholder="Nombre Empresa" required>
                         </div>
                         <div class="mb-3 col-6">
                             <label for="direccion" class="form-label">Direccion</label>
-                            <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Direccion">
+                            <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Direccion" required>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="mb-3 col-4">
+                        <div class="mb-3 col-6">
                             <label for="telefono" class="form-label">Telefono</label>
-                            <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono">
+                            <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono" required>
                         </div>
-                        <div class="mb-3 col-4">
+                        <div class="mb-3 col-6">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
                         </div>
-                        <div class="mb-3 col-4">
-                            <label for="asesor" class="form-label">Asesor</label>
-                            <input type="text" class="form-control" id="asesor" name="asesor" placeholder="Nombre Asesor">
-                        </div>
+
                     </div>
-                    
-                    <div class="mb-3">
-                        <label for="telAsesor" class="form-label">Telefono Asesor</label>
-                        <input type="text" class="form-control" id="telAsesor" name="telAsesor" placeholder="Telefono Asesor">
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <label for="asesor" class="form-label">Asesor</label>
+                            <input type="text" class="form-control" id="asesor" name="asesor" placeholder="Nombre Asesor" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="telAsesor" class="form-label">Telefono Asesor</label>
+                            <input type="number" class="form-control" id="telAsesor" name="telAsesor" placeholder="Telefono Asesor" required>
+                        </div>
+
                     </div>
                     <div class="mb-3">
                         <label for="in" class="form-label">Informacion Adicional</label>
-                        <textarea class="form-control" id="in" name="" rows="3"></textarea>
+                        <textarea class="form-control" id="in" name="infoAdicional" rows="3" required></textarea>
                     </div>
-                    <div class="mb-3">
-                        <input class="btn btn-primary" type="submit" value="Submit">
+                    <div class="mb-2 offset-md-5">
+                        <input class="btn btn-primary" name="guardar_proveedor" type="submit" value="Guardar">
                     </div>
                 </div>
             </div>
         </div>
     </form>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
