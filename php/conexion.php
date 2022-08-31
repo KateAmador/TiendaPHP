4aaea59956a5869499dfaa376dadd666248d4990
ronal2003 <ronuldox.2003@gmail.com>
2022-08-31 15:13:45 -0500

    [Cliente-vista]: se sube formulario cliente con la conexion a base de datos

--- php/conexion.php
+++ php/conexion.php
@@ -9,30 +9,6 @@ $password = "lPaw8OcTpppjjKllEvO0";
 $conn = mysqli_connect($servername, $username, $password, $database);
 
   
-// Check connection
-if (!$conn) {
-    die("Fallo la conexion: " . mysqli_connect_error());
-}
-echo "Conexion exitosa<br>";
-//mysqli_close($conn);
-
-
-	# Comprobar si existe registro
-
-    // $usertable="prueba";
-    // $yourfield = "nombre";
-	
-	// $query = "SELECT * FROM $usertable";
-	
-	// $result = mysqli_query($conn, $query);
-
-    // if($result){
-	// 	while($row = mysqli_fetch_array($result)){
-	// 		$name = $row["$yourfield"];
-	// 		echo "Nombre: ".$name."<br>";
-	// 	}
-	// }
-
 
 ?>
 
