e10a236a24fba850a9f95166c0721ba608699e3f
Kamador <kateamador13@gmail.com>
2022-09-02 01:03:18 -0500

    [proveedor]: se muestran los registros en la tabla

--- php/crudProveedor/proveedor.controlador.php
+++ php/crudProveedor/proveedor.controlador.php
@@ -1,5 +1,13 @@
 <?php
  
+ if(isset($_POST['guardar_proveedor'])) {
+    guardar();
+} else if(isset($_POST['eliminar'])) {
+
+}
+
+ function guardar(){
+
     include_once('../conexion.php');
     $nombreEmpresa = trim($_POST['nombreEmpresa']);
     $direccion = trim($_POST['direccion']);
@@ -13,11 +21,28 @@
     $sql= "INSERT INTO proveedores(empresa_proveedor, direccion_proveedor, telefono_proveedor, email_proveedor, asesor_proveedor, telAsesor_proveedor, infoAdicional_proveedor)
              VALUES ('$nombreEmpresa','$direccion','$telefono','$email','$asesor','$telAsesor','$infoAdicional')";
     if (mysqli_query($conn, $sql)) {
-        echo "Proveedor creado correctamente";
+        echo '<div class="alert alert-success">Proveedor creado correctamente</div>';
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
     
     mysqli_close($conn);
+ }
 
+
+
+
+    if (isset($_GET["id_proveedor"])){
+        $id=$_GET["id_proveedor"];
+        
+        $sql="DELETE FROM proveedores WHERE id_proveedor=$id";
+        if (mysqli_query($conn, $sql)) {
+            echo '<div class="alert alert-success">Proveedor eliminado correctamente</div>';
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+
+ 
+ 
 ?>
--- php/crudProveedor/proveedor.vista.php
+++ php/crudProveedor/proveedor.vista.php
@@ -12,52 +12,56 @@
 </head>
 
 <body>
-    <?php include '../util/header.php'; ?>
-    <div class="container-fluid row">
-        <div class="card col-6 offset-3 mt-5">
+    <?php include '../util/header.php';  ?>
+    <div class="text-center mt-10">
+        <br><br>
+        <h3>PROVEEDORES</h3>
+    </div>
+    <div class="container-fluid row ">
+        <div class="card col-4 mt-5 shadow-sm p-3 mb-5 bg-white rounded">
             <form method="POST" action="proveedor.controlador.php">
-                <div class="card-header">
+                <div class="card-header bg-secondary text-white">
                     <h5 class="card-title text-center">Agregar Proveedor</h5>
                 </div>
                 <div class="card-body">
                     <div>
-                        <div class="row">
-                            <div class="mb-3 col-6">
+                        <div class="">
+                            <div class="mb-2 col-11">
                                 <label for="nombreEmpresa" class="form-label">Empresa</label>
                                 <input type="text" class="form-control" id="nombreEmpresa" name="nombreEmpresa" placeholder="Nombre Empresa" required>
                             </div>
-                            <div class="mb-3 col-6">
+                            <div class="mb-2 col-11">
                                 <label for="direccion" class="form-label">Direccion</label>
                                 <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Direccion" required>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="mb-3 col-6">
+                        <div class="">
+                            <div class="mb-2 col-11">
                                 <label for="telefono" class="form-label">Telefono</label>
                                 <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono" required>
                             </div>
-                            <div class="mb-3 col-6">
+                            <div class="mb-2 col-11">
                                 <label for="email" class="form-label">Email</label>
                                 <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
                             </div>
 
                         </div>
-                        <div class="row">
-                            <div class="mb-3 col-6">
+                        <div class="">
+                            <div class="mb-2 col-11">
                                 <label for="asesor" class="form-label">Asesor</label>
                                 <input type="text" class="form-control" id="asesor" name="asesor" placeholder="Nombre Asesor" required>
                             </div>
-                            <div class="mb-3 col-6">
+                            <div class="mb-2 col-11">
                                 <label for="telAsesor" class="form-label">Telefono Asesor</label>
                                 <input type="number" class="form-control" id="telAsesor" name="telAsesor" placeholder="Telefono Asesor" required>
                             </div>
 
                         </div>
-                        <div class="mb-5">
+                        <div class="mb-2 col-11">
                             <label for="in" class="form-label">Informacion Adicional</label>
-                            <textarea class="form-control" id="in" name="infoAdicional" rows="3" required></textarea>
+                            <textarea class="form-control" id="in" name="infoAdicional" rows="2" required></textarea>
                         </div>
-                        <div class="mb-5 offset-md-5">
+                        <div class="mb-2">
                             <input class="btn btn-primary" name="guardar_proveedor" type="submit" value="Guardar">
                         </div>
                     </div>
@@ -65,10 +69,10 @@
                 </div>
             </form>
         </div>
-        <div class="col-12 mt-5">
-            <table class="table">
+        <div class="col-8 mt-5">
+            <table class="table table table-striped shadow-sm p-3 mb-5 bg-white rounded">
                 <thead>
-                    <tr>
+                    <tr class="bg-secondary text-white">
                         <th scope="col">#</th>
                         <th scope="col">Empresa</th>
                         <th scope="col">Direccion</th>
@@ -81,20 +85,26 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Mark</td>
-                        <td>
-                            <a href="" class="btn btn-small btn-warning"><i class="fa-solid fa-pen"></i></a>
-                            <a href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></a>
-                        </td>
-                    </tr>
+                    <?php
+                    include_once('../conexion.php');
+                    $sql = $conn->query("SELECT * FROM proveedores");
+                    while ($datos = $sql->fetch_object()) {
+                    ?>
+                        <tr>
+                            <th scope="row"><?= $datos->id_proveedor; ?></th>
+                            <td><?= $datos->empresa_proveedor; ?></td>
+                            <td><?= $datos->direccion_proveedor; ?></td>
+                            <td><?= $datos->telefono_proveedor; ?></td>
+                            <td><?= $datos->email_proveedor; ?></td>
+                            <td><?= $datos->asesor_proveedor; ?></td>
+                            <td><?= $datos->telAsesor_proveedor; ?></td>
+                            <td><?= $datos->infoAdicional_proveedor; ?></td>
+                            <td>
+                                <a href="" class="btn btn-small btn-warning"><i class="fa-solid fa-pen"></i></a>
+                                <a href="proveedor.vista.php?id=<?= $datos->id_proveedor ?>" class="btn btn-small btn-danger" name="eliminar" type="submit"><i class="fa-solid fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
 
@@ -102,4 +112,5 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/602acc29db.js" crossorigin="anonymous"></script>
+
 </html>
