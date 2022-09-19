c17941232bd402badd57e7ad387955102502d60e
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-09-19 16:09:17 -0500

    [producto]: se suben todos los cambios

--- crudProducto/producto.vista.php
+++ crudProducto/producto.vista.php
@@ -3,9 +3,89 @@ include_once "../components/options.component.php";
 $component = new Component();
 ?>
 
-<?php include '../util/header.php'; ?>
+<body>
+
+    <form method="POST" action="producto.controlador.php">
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header bg-secondary text-white">
+                        <h5 class="modal-title" id="exampleModalLabel">Editar Producto</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            <div class="mb-3 col-2">
+                                <label for="id" class="form-label">ID</label>
+                                <input type="text" class="form-control" id="id" name="id" readonly
+                                    onmousedown="return false;">
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col-6">
+                                    <label for="nombreProducto" class="form-label">Nombre</label>
+                                    <input type="text" class="form-control" id="nombreProducto" name="nombreProducto"
+                                        placeholder="Nombre Producto" required>
+                                </div>
+                                <div class="mb-3 col-6">
+                                    <label for="costoProducto" class="form-label">Costo</label>
+                                    <input type="number" class="form-control" id="costoProducto" name="costoProducto"
+                                        placeholder="Costo Producto" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col-6">
+                                    <label for="precioProducto" class="form-label">Precio Venta</label>
+                                    <input type="number" class="form-control" id="precioProducto" name="precioProducto"
+                                        placeholder="Precio Producto" required>
+                                </div>
+                                <div class="mb-3 col-6">
+                                    <label for="nombreProveedor" class="form-label">Proveedor</label>
+                                    <input type="text" class="form-control" id="nombreProveedor" name="nombreProveedor"
+                                        placeholder="Nombre Proveedor" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mb-3 col-6">
+                                    <label for="nombreCategoria" class="form-label">Categoria</label>
+                                    <input type="text" class="form-control" id="nombreCategoria" name="nombreCategoria"
+                                        placeholder="Nombre Cateogria" required>
+                                </div>
+                                <div class="mb-3 col-6">
+                                    <label for="estadoProducto" class="form-label">Estado del Producto</label>
+                                    <input type="number" class="form-control" id="estadoProducto" name="estadoProducto"
+                                        placeholder="estadoProducto" required>
+                                </div>
+                            </div>
+                            <div class="mb-3 col-6">
+                                <label for="cantidadProducto" class="form-label">Estado del Producto</label>
+                                <input type="number" class="form-control" id="cantidadProducto" name="cantidadProducto"
+                                    placeholder="Cantidad Producto" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="observaciones" class="form-label">Observcion del Producto</label>
+                                <input type="text" name="observaciones" class="form-control" id="observaciones"
+                                    placeholder="Observacion">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <input class="btn btn-secondary" name="actualizar_proveedor" type="submit" value="Guardar">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+
+
+    <?php include '../util/header.php';  ?>
+
+
 
     <form class="card col-12 col-md-6 offset-md-2 mt-5" method="post" action="producto.controlador.php">
+
+
         <div class="card-header">
             <h5 class="card-title">Agregar Producto</h5>
         </div>
@@ -16,35 +96,37 @@ $component = new Component();
                 <div class="col-md-6 col-lg-6 col-xl-12 order-2 order-lg-1">
                     <div class="mb-3">
                         <label for="nombreProducto" class="form-label">Nombre</label>
-                        <input type="text" name = "nombreProducto" class="form-control" id="exampleFormControlInput1"
+                        <input type="text" name="nombreProducto" class="form-control" id="exampleFormControlInput1"
                             placeholder="Nombre Producto">
-                    </div>               
-                    <div class="row">                 
+                    </div>
+                    <div class="row">
                         <div class="mb-3  col-6">
                             <label for="costoProducto" class="form-label">Costo</label>
-                            <input type="number" name ="costoProducto" class="form-control" id="exampleFormControlInput1" placeholder="Valor de Compra">
+                            <input type="number" name="costoProducto" class="form-control" id="exampleFormControlInput1"
+                                placeholder="Valor de Compra">
                         </div>
                         <div class="mb-3  col-6">
                             <label for="precioProducto" class="form-label">Precio Venta</label>
-                            <input type="number" name="precioProducto" class="form-control" id="exampleFormControlInput1" placeholder="Valor de Venta">
+                            <input type="number" name="precioProducto" class="form-control"
+                                id="exampleFormControlInput1" placeholder="Valor de Venta">
                         </div>
                     </div>
-                <div class="mb-3 col-12">
-                    <label for="proveedor" class="form-label">Proveedor</label>
-                    <select class="form-select" aria-label="Default select example" name="nombreProveedor">
-                        <option selected>Proveedores</option>
-                        <?php $component->viewOptionsComponent("proveedores", "0", "1") ?>
-                    </select>
-                </div>
-                <div class="mb-3 col-12">
-                    <label for="proveedor" class="form-label">Categoria</label>
-                    <select class="form-select" aria-label="Default select example" name="nombreCategoria">
-                        <option selected>Categoria</option>
-                        <?php $component->viewOptionsComponent("categorias", "0", "1") ?>
-                    </select>
-                </div>
+                    <div class="mb-3 col-12">
+                        <label for="proveedor" class="form-label">Proveedor</label>
+                        <select class="form-select" aria-label="Default select example" name="nombreProveedor">
+                            <option selected>Proveedores</option>
+                            <?php $component->viewOptionsComponent("proveedores", "0", "1") ?>
+                        </select>
+                    </div>
+                    <div class="mb-3 col-12">
+                        <label for="proveedor" class="form-label">Categoria</label>
+                        <select class="form-select" aria-label="Default select example" name="nombreCategoria">
+                            <option selected>Categoria</option>
+                            <?php $component->viewOptionsComponent("categorias", "0", "1") ?>
+                        </select>
+                    </div>
 
-                    <div class="row"> 
+                    <div class="row">
 
                         <div class="mb-3  col-6">
                             <label for="estadoProducto" class="form-label">Estado del Producto</label>
@@ -56,8 +138,9 @@ $component = new Component();
                         </div>
                         <div class="mb-3  col-6">
                             <label for="cantidadProducto" class="form-label">Cantidad del Producto</label>
-                            <input type="number" name="cantidadProducto" class="form-control" id="exampleFormControlInput1" placeholder="Valor de Compra">
-                        </div>     
+                            <input type="number" name="cantidadProducto" class="form-control"
+                                id="exampleFormControlInput1" placeholder="Valor de Compra">
+                        </div>
                     </div>
                     <div class="mb-3">
                         <label for="Observacion" class="form-label">Observcion del Producto</label>
@@ -66,12 +149,24 @@ $component = new Component();
                     </div>
 
                     <div class="mb-3">
-                    <input class="btn btn-primary" name="guardar_producto" type="submit" value="Guardar">
+                        <input class="btn btn-primary" name="guardar_producto" type="submit" value="Guardar">
                     </div>
                 </div>
             </div>
         </div>
     </form>
+
+
+
+
+
+
+
+
+   
+
+
+
 
 
 
@@ -124,10 +219,8 @@ $component = new Component();
                 <td><?= $datos->estado_producto; ?></td>
                 <td><?= $datos->cantidad_producto; ?></td>
                 <td>
-                    <a href="" class="btn btn-small btn-warning"
-                        onclick="agregarForm('<?php echo $datosProducto ?>');"><i class="fa-solid fa-pen"></i></a>
-                    <a href="poducto.controlador.php?id=<?= $datos->id_producto ?>" class="btn btn-small btn-danger"><i
-                            class="fa-solid fa-trash"></i></a>
+                <a href="" class="btn btn-small btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="agregarForm('<?php echo $datosProducto ?>');"><i class="fa-solid fa-pen"></i></a>
+                <a href="producto.controlador.php?id=<?= $datos->id_producto ?>" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></a>
                 </td>
             </tr>
             <?php } ?>
@@ -136,7 +229,6 @@ $component = new Component();
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script>
-
     function agregarForm(datos) {
         d = datos.split("||");
 
@@ -154,4 +246,3 @@ $component = new Component();
     </script>
 
     <?php include '../util/footer.php';  ?>
-
